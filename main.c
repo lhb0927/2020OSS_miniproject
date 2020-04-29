92eea6f17440eaeb3f803fd50749b7daa6a41c3d
taeyoung <taeyounglee19@gmail.com>
2020-04-29 09:17:45 +0900

    main.c: main function added

---
+++ main.c
@@ -0,0 +1,29 @@
+#include "product.h"
+
+int main(void){
+	int menu, count, curcount;
+	while(1){
+		menu=cafemenu();
+		if(menu==0) break;
+		else if(menu==1){
+
+		} 
+		else if(menu==2){
+
+		} 
+		else if(menu==3){
+
+		} 
+		else if(menu==4){
+
+		} 
+		else if(menu==5){
+
+		} 
+		else if(menu==6){
+
+		} 
+	}
+	printf("=> 프로그램 종료됨\n");
+	return 0;
+}
