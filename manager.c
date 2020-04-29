3ab8064930aac7a35a48bd55b4303a932e951dca
Lim hyeongbin <lhb0927@naver.com>
2020-04-29 15:06:31 +0900

    manager.c: create load/save functions and search functions

---
+++ manager.c
@@ -0,0 +1,25 @@
+#include "manager.h"
+
+int loadData(MENU t[]){
+
+}
+
+void saveData(MENU t[],int count){
+
+}
+
+void searchName(MENU *t,int count){
+
+}
+
+void searchPrice(MENU *t,int count){
+
+}
+
+void searchSize(MENU *t,int count){
+
+}
+
+void searchType(MENU *t,int count){
+
+}
