45023fa61c1b1ae25602cb8c47037e0f75627f8b
Lim hyeongbin <lhb0927@naver.com>
2020-05-21 17:45:57 +0900

    Edit hello.c

--- hello.c
+++ hello.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
 int main(){
-  printf("Hello World!!!\n");
-  printf("I'm Hyeongbin.\n");
-  
-  return 0;
+  	printf("Hello World!!!\n");
+	printf("Handong Global University\n");
+	printf("CSEE\n");
+	printf("Name : Lim  Hyeongbin.\n");
+	printf("Studint ID : 21500612\n");
+	return 0;
 }
